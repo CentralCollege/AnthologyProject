6ccbc5a731b10c17a30fbae252c7e727360f744e
Andrew M Gorman <gormana1@central.edu>
2018-11-20 13:57:18 -0600

    Uploading last work

--- functions.php
+++ functions.php
@@ -15,6 +15,35 @@ add_action( 'wp_enqueue_scripts', 'cui_add_styles' );
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 200, 200, array( 'center', 'center')  );
 
+add_filter( 'acf/fields/wysiwyg/toolbars' , 'my_toolbars'  );
+function my_toolbars( $toolbars )
+{
+	// Uncomment to view format of $toolbars
+	/*
+	echo '< pre >';
+		print_r($toolbars);
+	echo '< /pre >';
+	die;
+	*/
+
+	// Add a new toolbar called "Very Simple"
+	// - this toolbar has only 1 row of buttons
+	$toolbars['Very Simple' ] = array();
+	$toolbars['Very Simple' ][1] = array('bold' , 'italic' , 'underline' );
+
+	// Edit the "Full" toolbar and remove 'code'
+	// - delet from array code from http://stackoverflow.com/questions/7225070/php-array-delete-by-value-not-key
+	if( ($key = array_search('code' , $toolbars['Full' ][2])) !== false )
+	{
+	    unset( $toolbars['Full' ][2][$key] );
+	}
+
+	// remove the 'Basic' toolbar completely
+	unset( $toolbars['Basic' ] );
+
+	// return $toolbars - IMPORTANT!
+	return $toolbars;
+}
 
 // ------------------------------------------------------------------------
 // Custom post type for alumni events
