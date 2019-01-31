c2a89fcd228ac3a988c88474c3ab0044448ba700
Andrew M Gorman <gormana1@central.edu>
2019-01-31 14:58:07 -0600

    Adds about page, adds issue for 2018

--- single-pastissues.php
+++ single-pastissues.php
@@ -84,7 +84,7 @@
           <div class="twelve columns">
             <h1>The Writing Anthology - <?php the_title(); ?></h1>
 						<h3>A Publication of the English Department and the Art Department</h3>
-						<p><a href="/writing-anthology/" class="button gaHero">Home</a><a href="/writing-anthology/past-issues/" class="button gaHero">Past Issues</a></p>
+						<p><a href="/writing-anthology/about/" class="button gaHero">About</a><a href="/writing-anthology/" class="button gaHero">Home</a><a href="/writing-anthology/past-issues/" class="button gaHero">Past Issues</a></p>
 						<!-- <a href="" class="button gaHero">Current Issue</a> -->
         </div>
       </section>
