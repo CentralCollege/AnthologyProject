04b083c6cf7956dc19d1aa57a4417dfe9de2c291
Andrew M Gorman <gormana1@central.edu>
2019-02-13 15:34:33 -0600

    Replaces footer with admission version, closes #7

--- footer.php
+++ footer.php
@@ -1,41 +1,37 @@
 </main>
 <footer>
-   <div class="container">
-       <div class="one-third column">
-         <h5>I'm looking for:</h5>
-         <ul>
-           <li><a href="https://central.textbookx.com/">Bookstore</a></li>
-             <li><a href="/campus-map">Campus Map</a></li>
-             <li><a href="/job-seekers">Careers</a></li>
-             <li><a href="https://departments.central.edu/diningservices/catering/">Catering Services</a></li>
-             <li><a href="https://www.centralspiritshoppe.com/">Central Spirit Shoppe</a></li>
-             <li><a href="/event-planners/">Conferences and Events</a></li>
-             <li><a href="/library">Library</a></li>
-             <li><a href="https://news.central.edu">News</a></li>
-             <li><a href="/campus-life/safety-at-central/">Safety</a></li>
-         </ul>
-       </div>
-       <div class="one-third column">
-         <div id="connect">
-           <h5>Contact Us</h5>
-             <p>Central College<br />
-             812 University<br />
-             Pella, Iowa 50219</p>
-             <p>1-800-447-0287<br />
-             <a href="mailto:alumni@central.edu">alumni@central.edu</a></p>
-         </div>
-       </div>
-       <div class="one-third column">
-         <p><a href="/"><img src="//d1lqhpmxg10s5j.cloudfront.net/images/main/footerLogo.png" alt="Central College" width="125" height="66" class="footerLogo"></a></p>
-         <div class="socialIcons">
-           <a href="https://www.facebook.com/centralcollege/" aria-label="Link to Facebook"><i class="fa fa-lg fa-facebook-official" title="Central College Facebook"></i></a>
-           <a href="https://twitter.com/centralcollege/" aria-label="Link to Twitter"><i class="fa fa-lg fa-twitter" title="Central College Twitter"></i></a>
-           <a href="https://instagram.com/centralcollege" aria-label="Link to Instagram"><i class="fa fa-lg fa-instagram" title="Central College Instagram"></i></a>
-           <a href="https://youtube.com/centralcollegeadm" aria-label="Link to youTube"><i class="fa fa-lg fa-youtube-square" title="Central College youTube"></i></a></p>
-         </div>
-         <p align="center"><a href="/policies/policy/678">Privacy Policy</a></p>
-       </div>
-         <div class="clearBoth"></div>
-     </div>
+  <div id="footer">
+    <div id="lookingFor">
+      <h5>I'm looking for:</h5>
+      <ul>
+        <li><a href="http://central.textbookx.com/">Bookstore</a></li>
+        <li><a href="/campus-map">Campus Map</a></li>
+        <li><a href="/job-seekers">Careers</a></li>
+        <li><a href="//departments.central.edu/diningservices/catering/">Catering Services</a></li>
+        <li><a href="https://www.centralspiritshoppe.com/">Central Spirit Shoppe</a></li>
+        <li><a href="http://events.central.edu/" target="_blank">Events</a></li>
+        <li><a href="/library">Library</a></li>
+        <li><a href="//news.central.edu">News</a></li>
+        <li><a href="/campus-life/safety-at-central/">Safety</a></li>
+      </ul>
+    </div>
+
+    <div id="connect">
+        <h5>Contact Us</h5>
+          <p>Central College</p>
+          <p>812 University St.</p>
+          <p>Pella, Iowa 50219</p>
+          <p>&nbsp;</p>
+          <p>877-462-3687</p>
+          <p><a href="mailto:admission@central.edu">admission@central.edu</a></p>
+    </div>
+
+    <div id="contactInfo">
+      <a href="/"><img src="//d1lqhpmxg10s5j.cloudfront.net/images/main/footerLogo.png" alt="Central College" width="125" height="66" class="footerLogo"></a>
+      <p class="alignCenter socialIcons"><a href="https://www.facebook.com/CentralCollege"><i class="fa fa-2x fa-facebook-official" aria-hidden="true"></i> <span class="sr-only">Central College Facebook</span></a> <a href="https://twitter.com/CentralCollege"><i class="fa fa-2x fa-twitter" aria-hidden="true"></i> <span class="sr-only">Central College Twitter</span></a> <a href="https://instagram.com/centralcollege"><i class="fa fa-2x fa-instagram" aria-hidden="true"></i><span class="sr-only">Central College Instagram</span></a> <a href="http://www.youtube.com/user/centralcollegeadm"><i class="fa fa-2x fa-youtube-square" aria-hidden="true"></i> <span class="sr-only">Central College youTube</span></a></p>
+      <p class="alignCenter"><a href="/policies/policy/678">Privacy Policy</a></p>
+    </div>
+    <div class="clearBoth"></div>
+  </div>
  </footer>
 <?php wp_footer();?>
