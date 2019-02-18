dec4b04b1c2d321e7490f285c3896dafe90925ab
Andrew M Gorman <gormana1@central.edu>
2019-02-18 15:36:55 -0600

    Adds mobile specific footer, closes #14

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer>
-  <div class="container centerFooter">
+  <div class="container centerFooter desktopFooter">
     <div class="four columns">
       <h3 class="footerLabel">I'm looking for</h3>
       <ul>
@@ -29,6 +29,40 @@
         <p class="alignCenter"><a href="/policies/policy/678">Privacy Policy</a></p>
       </div>
       <div class="clearBoth"></div></div>
+      <div class="container centerFooter mobileFooter">
+        <div class="row">
+        <div class="six columns">
+          <h3 class="footerLabel">I'm looking for</h3>
+          <ul>
+            <li><a href="http://central.textbookx.com/">Bookstore</a></li>
+            <li><a href="/campus-map">Campus Map</a></li>
+            <li><a href="/job-seekers">Careers</a></li>
+            <li><a href="//departments.central.edu/diningservices/catering/">Catering Services</a></li>
+            <li><a href="https://www.centralspiritshoppe.com/">Central Spirit Shoppe</a></li>
+            <li><a href="http://events.central.edu/" target="_blank">Events</a></li>
+            <li><a href="/library">Library</a></li>
+            <li><a href="//news.central.edu">News</a></li>
+            <li><a href="/campus-life/safety-at-central/">Safety</a></li>
+          </ul>
+        </div>
+        <div class="six columns">
+          <h3 class="footerLabel">Contact Us</h3>
+          <p>Central College<br />812 University St.<br />Pella, Iowa 50219</p>
+          <p>877-462-3687<br /><a href="mailto:admission@central.edu">admission@central.edu</a></p>
+        </div>
+        </div>
+        <div class="row">
+          <div class="twelve columns">
+            <a href="/"><img src="//d1lqhpmxg10s5j.cloudfront.net/images/main/footerLogo.png" alt="Central College" width="125" height="66" class="footerLogo"></a>
+            <p class="alignCenter socialIcons"><a href="https://www.facebook.com/CentralCollege"><i class="fab fa-2x fa-facebook-square" aria-hidden="true"></i> <span class="sr-only">Central College Facebook</span></a>
+              <a href="https://twitter.com/CentralCollege"><i class="fab fa-2x fa-twitter-square" aria-hidden="true"></i> <span class="sr-only">Central College Twitter</span></a>
+              <a href="https://instagram.com/centralcollege"><i class="fab fa-2x fa-instagram" aria-hidden="true"></i><span class="sr-only">Central College Instagram</span></a>
+              <a href="http://www.youtube.com/user/centralcollegeadm"><i class="fab fa-2x fa-youtube-square" aria-hidden="true"></i> <span class="sr-only">Central College youTube</span></a></p>
+            <p class="alignCenter"><a href="/policies/policy/678">Privacy Policy</a></p>
+          </div>
+        </div>
+      <div class="clearBoth"></div>
+    </div>
     </div>
   </footer>
 <?php wp_footer();?>
