097b3adb98a3620c877725dd3fb0bbfd3280b688
Andrew M Gorman <gormana1@central.edu>
2019-02-21 15:28:34 -0600

    Final touches on new theme

--- single-pastissues.php
+++ single-pastissues.php
@@ -32,25 +32,25 @@ get_header();
 				while ( $the_query->have_posts() ) {
 					$the_query->the_post();
 					?>
-					<a href="<?php the_permalink()?>">
 						<div class="row">
 							<div class="twelve columns story">
-								<h2 class="articleTitle"><?php the_title();?></h2>
+								<a href="<?php the_permalink()?>" class="articleTitle" style="text-decoration: none;"><h2><?php the_title();?></h2></a>
 								<?php
 								if ( has_post_thumbnail() ) {
-									?> <div class="three columns"><?php
+									?> <a href="<?php the_permalink()?>"><div class="three columns"><?php
 									the_post_thumbnail('thumbnail', array('class' => 'singlePostImage'));
-									?> </div> <?php
+									?> </div></a> <?php
 								}
-								?><div class="eight columns">
-										<p class="studentAuthor"><?php
+								?><div class="nine columns">
+										<h4 class=""><?php
 										the_field('student_author');
-									?></p>
-									<p class="authorsNote"><?php the_field('authors_note'); ?></p>
+									?></h4>
+									<p class="authorsNote"><?php the_excerpt(); ?></p>
+									<a href="<?php the_permalink();?>" class="redButton">Read More</a>
 									</div>
 							</div>
 						</div>
-					</a><?php
+					<?php
 				}
 			}
 			?>
